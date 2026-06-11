d7ab98f80c1bfead4bdc4ab2ab91244eae6033c3
aishwarya14-dev <aishwaryamehrotra14@gmail.com>
2026-06-11 16:27:31 +0530

    remove health check config

--- src/main/java/com/aishwarya/FinBank/config/HealthCheckConfig.java
+++ src/main/java/com/aishwarya/FinBank/config/HealthCheckConfig.java
@@ -6,19 +6,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
-@Component
-public class HealthCheckConfig {
-
-    @Value("${spring.data.redis.url:NOT_FOUND}")
-    private String redisUrl;
-
-    @Value("${spring.datasource.url:NOT_FOUND}")
-    private String dbUrl;
-
-    @PostConstruct
-    public void init(){
-        System.out.println("REDIS URL = " + redisUrl);
-        System.out.println("DATABASE URL = " + dbUrl);
-        System.out.println("JWT = " + System.getenv("JWT_SECRET"));
-    }
-}
+//@Component
+//public class HealthCheckConfig {
+//
+//    @Value("${spring.data.redis.url:NOT_FOUND}")
+//    private String redisUrl;
+//
+//    @Value("${spring.datasource.url:NOT_FOUND}")
+//    private String dbUrl;
+//
+//    @PostConstruct
+//    public void init(){
+//        System.out.println("REDIS URL = " + redisUrl);
+//        System.out.println("DATABASE URL = " + dbUrl);
+//        System.out.println("JWT = " + System.getenv("JWT_SECRET"));
+//    }
+//}
